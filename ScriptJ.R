4e1067b3fc73168f17dfa12ae37ff8a3597cb9be
joaomarcuspl <joao.marcusbio@gmail.com>
2026-06-17 15:19:02 -0300

    atualização joao

--- ScriptJ.R
+++ ScriptJ.R
@@ -1,1 +1,4 @@
-novo script João
+#novo script João
+
+#Introdução: besouros Scarabaeinae podem utilizar frutas como recurso, além das fezes e carcaças
+#Metodologia: Pitfall com frutas para coleta de Scarabaenae
